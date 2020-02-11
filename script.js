31f8bbd89bb4daa96f77e6654ad1377f6aa0aa81
KantKontrol <nick.derissio.tech@gmail.com>
2020-02-10 21:09:42 -0500

    added call to uvindex api and appended data to screen

--- script.js
+++ script.js
@@ -26,28 +26,50 @@ function getWeather(city){
 
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2f13881ffe862b42e36007854de27a99&units=imperial"
 
-    $.ajax({
+    let weatherToday;
+    let uvIndex;
+
+    $.ajax({ //weather for today
         url: queryURL,
         method: "GET"
     }).then(function(response){
 
-        console.log(response);
+        weatherToday = response;
 
-        currentWeather(response);
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+
+        getUV(weatherToday, lat, lon);
 
     });
 
-
 }
 
-function currentWeather(response){
+function getUV(weatherToday, lat, lon){
 
-    $("#cityTag").html(response.name);
-    $("#dateTag").html();
+    let queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=2f13881ffe862b42e36007854de27a99&lat=" + lat + "&lon=" + lon;
+
+
+    $.ajax({ //weather for today
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+
+        let uvIndex = response.value;
+        let date = response.date_iso;
+
+        currentWeather(weatherToday, uvIndex, date);
+    });
+}
+
+function currentWeather(response, uvIndex, date){
+
+    $("#cityTag").html(response.name + " (" + date + ")");
     $("#iconTag").html();
     $("#tempTag").html("Temperature: " + response.main.temp + " F");
     $("#humidityTag").html("Humidity: " + response.main.humidity);
     $("#windSpeed").html("Wind Speed: " + response.wind.speed + "mph");
-    $("#uvIndex").html("UV Index: ");
+    $("#uvIndex").html("UV Index: " + uvIndex);
 
 }

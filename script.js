6261ab26fa4d81f568931ad3e91b64d10ab2499c
KantKontrol <nick.derissio.tech@gmail.com>
2020-02-10 20:19:35 -0500

    added basic h5s for data and basic function

---
+++ script.js
@@ -0,0 +1,21 @@
+/* City
+
+* Date
+
+* Icon image (visual representation of weather conditions)
+
+* Temperature
+
+* Humidity
+
+* Wind speed
+
+* UV index*/
+
+getWeather();
+
+
+function getWeather(){
+
+
+}
